b2ca72be0fd8fa83ab1cd1123ed163fdac2bd711
Тарас Викторович (4067694) Ковалёв <kovalevtv@vtb.ru>
2025-10-30 11:06:01 +0300

    Добавить функцию поиска максимального и минимального числа в массиве, используя оптимизированный цикл прохода с двух сторон, и вывести результат на консоль.

--- tasks/task2/findMaxMin.ts
+++ tasks/task2/findMaxMin.ts
@@ -1,7 +1,36 @@
 import { generateAndWriteRandomNumbersToFile } from './generatorNumbers';
-
+import randomNumbers from './randomNumbers.json';
 
 false && generateAndWriteRandomNumbersToFile();
 
+type FindMaxMinResult = { max: number, min: number }
 
+const findMaxMin = (listNumbers: number[]): FindMaxMinResult => {
+  let max: number = listNumbers[0];
+  let min: number = listNumbers[0];
 
+  const count = listNumbers.length / 2;
+
+  for (let iBegin = 0, iEnd = listNumbers.length - 1; iBegin < count; iBegin++, iEnd--) {
+    const elBegin = listNumbers[iBegin];
+    const elEnd = listNumbers[iEnd];
+
+    if (elBegin > max) {
+      max = elBegin;
+    } else if (elBegin < min) {
+      min = elBegin;
+    }
+
+    if (elEnd > max) {
+      max = elEnd;
+    } else if (elEnd < min) {
+      min = elEnd;
+    }
+  }
+
+  return { max, min }
+}
+
+const result = findMaxMin(randomNumbers);
+
+console.log('result:', result)
