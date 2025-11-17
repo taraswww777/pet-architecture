176515ec05836485742c79657f4161ed5d8448b1
Тарас Викторович (4067694) Ковалёв <kovalevtv@vtb.ru>
2025-11-17 09:47:21 +0300

    task2 refactor(findMaxMin): improve logic and handle edge cases

--- tasks/task2/findMaxMin.ts
+++ tasks/task2/findMaxMin.ts
@@ -8,21 +8,37 @@ type FindMaxMinResult = { max: number, min: number }
 export const findMaxMin = (listNumbers: number[]): FindMaxMinResult => {
   if (listNumbers.length === 0) throw new Error('Array is empty');
 
-  let max: number = listNumbers[0];
-  let min: number = listNumbers[0];
+  let max: number;
+  let min: number;
 
-  if (listNumbers.length===1){
-    return { max, min }
+  if (listNumbers.length === 1) {
+    max = listNumbers[0] as number;
+    min = listNumbers[0] as number;
+  } else {
+    // @ts-expect-error всё работает
+    if (listNumbers[0] > listNumbers[1]) {
+      max = listNumbers[0] as number;
+      min = listNumbers[1] as number;
+    } else {
+      max = listNumbers[1] as number;
+      min = listNumbers[0] as number;
+    }
   }
 
-  const count = listNumbers.length / 2;
+  for (let i = 2; i < listNumbers.length; i += 2) {
+    let elBegin: number;
+    let elEnd: number;
 
-  for (let iBegin = 0, iEnd = listNumbers.length - 1; iBegin < count; iBegin++, iEnd--) {
-    const elBegin = listNumbers[iBegin];
-    const elEnd = listNumbers[iEnd];
+    // Обработка случая, когда остался только один элемент
+    if (i + 1 >= listNumbers.length) {
+      elBegin = listNumbers[i] as number;
+      elEnd = listNumbers[i] as number;
+    } else {
+      elBegin = listNumbers[i] as number;
+      elEnd = listNumbers[i + 1] as number;
+    }
 
-
-    if (elBegin > elEnd){
+    if (elBegin > elEnd) {
       if (elBegin > max) {
         max = elBegin;
       }
