9bb9a8fcd41bb21bbf51a1bc0b4c86b704013a68
Тарас Викторович (4067694) Ковалёв <kovalevtv@vtb.ru>
2025-11-26 15:31:40 +0300

    task3 version 2

--- tasks/task3/StructureCollection.ts
+++ tasks/task3/StructureCollection.ts
@@ -8,15 +8,22 @@ const defaultComparator: Comparator<number> = (a, b) => {
   return -1;
 };
 
+/**
+ * Коллекция, хранящая отсортированные значения.
+ */
 export class StructureCollection<TItem = number> {
-  private __listData: TItem[] = [];
+  private readonly __listData: TItem[] = [];
   private readonly __comparator: Comparator<TItem> = defaultComparator as Comparator<TItem>;
 
+  /**
+   * Создаёт новую коллекцию.
+   * @param initValue Начальные значения.
+   * @param comparator Компаратор для сортировки.
+   */
   constructor(initValue: TItem[] = [], comparator: Comparator<TItem> = defaultComparator as Comparator<TItem>) {
     this.__comparator = comparator;
-    initValue.forEach((item: TItem) => {
-      this.__listData.push(item);
-    })
+    initValue.sort(comparator);
+    this.__listData = initValue;
   }
 
   get listData() {
@@ -37,29 +44,45 @@ export class StructureCollection<TItem = number> {
     return this.__listData.splice(index, 0, newItem);
   }
 
-  __findIndexForInsertNewItemByValue(newValue: TItem) {
-    const length = this.__listData.length;
-    let index = length;
+  __findIndexForInsertNewItemByValue(newValue: TItem): number {
+    let low = 0;
+    let high = this.__listData.length;
 
-
-    if(length===0){
-      return index;
-    }
-
-    for (let i = 0; i < length; i++) {
+    while (low < high) {
+      const mid = Math.floor((low + high) / 2);
       // @ts-expect-error по факту всё будет работать
-      if(this.__comparator(this.__listData[i], newValue) >= 0){
-        index = i;
-        break;
+      if (this.__comparator(this.__listData[mid], newValue) < 0) {
+        low = mid + 1;
+      } else {
+        high = mid;
       }
     }
 
-    return index;
+    return low;
   }
 
-  findIndexByValue(value: TItem) {
-    return this.__listData.findLastIndex((item: TItem) => this.__comparator(item, value) === 0);
+
+  findIndexByValue(value: TItem): number {
+    let low = 0;
+    let high = this.__listData.length - 1;
+
+    while (low <= high) {
+      const mid = Math.floor((low + high) / 2);
+      // @ts-expect-error по факту всё будет работать
+      const comparison = this.__comparator(this.__listData[mid], value);
+
+      if (comparison === 0) {
+        return mid;
+      } else if (comparison < 0) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+
+    return -1;
   }
+
 
   removeAt(index: number) {
     return this.__listData.splice(index, 1);
